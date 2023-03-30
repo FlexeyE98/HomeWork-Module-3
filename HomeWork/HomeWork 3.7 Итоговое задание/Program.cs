b4c968b3fab8103761dda8674d4c38aa7bb7077b
Alexey Emelchikov <aleksemelchikov@gmail.com>
2023-03-30 21:19:05 +0300

    Итоговое задание юнита 3.7

---
+++ HomeWork/HomeWork 3.7 Итоговое задание/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HomeWork_3._7_Итоговое_задание
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите ваш возраст: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Ваше имя {name} и ваш возраст {age}");
+            Console.Write("Введите вашу дату рождения: ");
+            string birthday = Console.ReadLine();
+            Console.WriteLine($"Ваша дата рождения {birthday}");
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
