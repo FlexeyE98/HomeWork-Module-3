c525582fefff14042cbc0e50cf88fe3d8ba00822
Alexey Emelchikov <aleksemelchikov@gmail.com>
2023-03-30 19:40:46 +0300

    Работа с типами данных 3.3.2

---
+++ HomeWork/HomeWork 3.3.2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HomeWork_3._3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string name = "My name is Alexey";
+            int age = 24;
+            bool pet = true;
+            double sizeShoes = 43.5;
+            Console.WriteLine($"{name}\nMy age is {age}\nDo I have a pet? {pet}\nMy shoe size is {sizeShoes}");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
