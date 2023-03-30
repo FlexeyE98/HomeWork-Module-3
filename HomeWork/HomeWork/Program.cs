2f4398aee1e0e5e90df5afba45aa71aa75b60abc
Alexey Emelchikov <aleksemelchikov@gmail.com>
2023-03-30 19:34:49 +0300

    create variable name

---
+++ HomeWork/HomeWork/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "My name is Alexey";
+            Console.WriteLine(name);
+
+            Console.ReadKey();
+
+        }
+    }
+}
